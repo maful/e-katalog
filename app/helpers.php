1417419197077f683657e3f5d82be0d9214da8a5
maful <mafulprayoga@gmail.com>
2019-02-20 13:03:38 +0700

    Add helpers

---
+++ app/helpers.php
@@ -0,0 +1,32 @@
+<?php
+
+if ( ! function_exists('format_rupiah'))
+{
+    /**
+     * Convert to Format Rupiah
+     *
+     * @param    integer    required
+     * @return    string
+     */
+    function format_rupiah($data)
+    {
+        return 'Rp' . number_format($data, 2, ',', '.');
+    }
+}
+
+if ( ! function_exists('badge_boolean'))
+{
+    /**
+     * Badge Boolean
+     *
+     * @param    boolean    required
+     * @return    string
+     */
+    function badge_boolean($bool)
+    {
+        $text = ($bool == 1 ? 'Aktif' : 'Nonaktif');
+        $badge = ($bool == 1 ? 'success' : 'danger');
+
+        return '<span class="badge badge-'.$badge.'">'.$text.'</span>';
+    }
+}
